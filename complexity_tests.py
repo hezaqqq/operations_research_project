3b22b83760aa83e58263fa1d2d99f0b134ad9711
Timothée <66522009+timotheetacca@users.noreply.github.com>
2026-04-30 13:39:22 +0200

    add complexity tests logs

--- complexity_tests.py
+++ complexity_tests.py
@@ -7,30 +7,44 @@ def test():
     sizes = [10, 40, 10**2, 4*10**2, 10**3, 4*10**3, 10**4]
     runs = 100
 
-    print(f"{'n':>8} | {'North West (s)':>12} | {'Balas Hammer (s)':>12}")
-    print("-" * 45)
+    print(f"{'n':>8} | {'North West (s)':>14} | {'Balas Hammer (s)':>16}")
+    print("-" * 50)
 
-    for n in sizes:
-        total_nw = 0
-        total_bh = 0
+    with open("nw_logs.txt", "a") as nw_file, open("bh_logs.txt", "a") as bh_file:
 
-        for i in range(runs):
-            A, P, C = complexityTransportationProblems(n)
+        for n in sizes:
+            nw_file.write(f"\n=== n = {n} ===\n")
+            bh_file.write(f"\n=== n = {n} ===\n")
 
-            start = time.perf_counter()
-            north_west(P[:], C[:])
-            end = time.perf_counter() # time.clock() doesn't exist anymore
-            total_nw += (end - start)
+            total_nw = 0
+            total_bh = 0
 
-            start = time.perf_counter()
-            bh(A, P[:], C[:])
-            end = time.perf_counter()
-            total_bh += (end - start)
+            for _ in range(runs):
+                A, P, C = complexityTransportationProblems(n)
 
-        avg_nw = total_nw / runs
-        avg_bh = total_bh / runs
+                # North-West
+                start = time.perf_counter()
+                north_west(P[:], C[:])
+                end = time.perf_counter()
+                duration_nw = end - start
+                total_nw += duration_nw
+                nw_file.write(f"{duration_nw}\n")
 
-        print(f"{n:8} | {avg_nw:14.6f} | {avg_bh:16.6f}")
+                # Balas-Hammer
+                start = time.perf_counter()
+                bh(A, P[:], C[:])
+                end = time.perf_counter()
+                duration_bh = end - start
+                total_bh += duration_bh
+                bh_file.write(f"{duration_bh}\n")
+
+            avg_nw = total_nw / runs
+            avg_bh = total_bh / runs
+
+            print(f"{n:8} | {avg_nw:14.6f} | {avg_bh:16.6f}")
+
+            nw_file.write(f"AVG: {avg_nw}\n")
+            bh_file.write(f"AVG: {avg_bh}\n")
 
 if __name__ == "__main__":
     test()
